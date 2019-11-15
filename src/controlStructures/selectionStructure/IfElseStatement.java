77a35c878a61b85b54ea0efc0e6e83023115d4c3
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2019-11-15 23:17:12 +0100

    If - Else Statement

---
+++ src/controlStructures/selectionStructure/IfElseStatement.java
@@ -0,0 +1,14 @@
+package controlStructures.selectionStructure;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int numberOne = 8;
+        int numberTwo = 10;
+
+        if(numberOne>numberTwo){
+            System.out.println("Number one greater than number two");
+        }else {
+            System.out.println("Number two greater than number one");
+        }
+    }
+}
