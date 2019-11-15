00a130b2f423c30b2c59da0cd27fac82f4af235e
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2019-11-15 23:11:47 +0100

    Selection structure added

---
+++ src/controlStructures/SelectionStructure.java
@@ -0,0 +1,8 @@
+package controlStructures;
+
+public class SelectionStructure {
+    public static void main(String[] args) {
+        System.out.println("Selection Structures : ");
+        System.out.println("if , if-else , switch case");
+    }
+}
