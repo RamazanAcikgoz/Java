b5a6325fca485ebd2f25d367e7a1adc10d7d8ba4
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2019-11-15 23:14:46 +0100

    If statement

---
+++ src/controlStructures/selectionStructure/IfStatement.java
@@ -0,0 +1,12 @@
+package controlStructures.selectionStructure;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int firstNumber = 8;
+        int secondNumber = 5;
+
+        if(firstNumber>secondNumber){
+            System.out.println("First value greater than second value");
+        }
+    }
+}
