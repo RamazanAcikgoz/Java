a15f4f304592e5e4eedf55bc03e2cf581ad606b7
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2020-04-15 14:15:23 +0200

    Collections&Maps

---
+++ src/collections/Maps.java
@@ -0,0 +1,14 @@
+package collections;
+
+//                                     MAP (INTERFACE)
+//
+//        SortedMap(INTERFACE)                          //HashMap(CLASS)
+//
+//           TreeMap(CLASS                            //LinkedHashMap(CLASS)
+//                                                      //HashTable(CLASS)
+//
+//      SortedMap extends MAP                               //HashMap implements MAP
+//    TreeMap implements SortedMap                       //LinkedHashMap extends HashMap
+//                                                             //HashTable implements MAP
+public class Maps {
+}
