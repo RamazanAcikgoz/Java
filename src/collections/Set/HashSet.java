84aa3786c5c3da5139107e4e0390ea0305409147
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2020-04-16 06:36:04 +0200

    Collections&Maps

---
+++ src/collections/Set/HashSet.java
@@ -0,0 +1,13 @@
+package collections.Set;
+//
+// HashSet class implements Set Interface.
+// It represents the collection that uses a hash table for storage.
+// Hashing is used to store the elements in the HashSet. It contains unique items.
+//
+// "HashMap" mantigi ile calisir. HashTable. Hizli arama ve guncelleme imkani saglar
+//
+public class HashSet {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/collections/Set/Sets.java
@@ -0,0 +1,15 @@
+package collections.Set;
+//
+// Kendisine verilen elemanlarin her birinden sadece bir tanesini tutar.
+// Tek kosul birbirinin ayni iki elemani ayni collection uzerinde bulundurmaz
+// Yani TEKRARLI yapilar bulunmaz
+//
+// It represents the unordered set of elements which doesn't allow us to store the duplicate items
+//
+// Bu INTERFACE'i kullanan tum sub-classlar bu kurala uymak zorundadir
+//
+public class Sets {
+    public static void main(String[] args) {
+
+    }
+}
